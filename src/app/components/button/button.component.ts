d6134faeceb638064719b10c02fd5302b4710407
Peter Won <peterwillknow@gmail.com>
2023-05-01 13:47:43 -0400

    migrating files

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent implements OnInit {
+  @Input() text: string = "";
+  @Input() color: string = "";
+  @Output() btnClick: EventEmitter<any> = new EventEmitter<any>();
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  onClick() {
+    this.btnClick.emit();
+  }
+
+}
